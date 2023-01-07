b81c8cc71e9fb6531eee733f8f9f5215586f23fd
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-01-08 00:22:31 +0900

    [Bronze V] Title: 윤년, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    int y;
+    std::cin >> y;
+    
+    if ((y % 4 == 0 && y % 100 != 0) || y % 400 == 0)
+        std::cout << "1";
+    else
+        std::cout << "0";
+}
