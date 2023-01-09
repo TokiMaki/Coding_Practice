bff5dfb21d6aa23270666f8547db61f2a437d625
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-01-09 15:32:04 +0900

    [Bronze V] Title: A+B - 7, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(){
+    int T, A, B;
+    
+    std::cin >> T;
+    
+    for(int i = 0; i < T; ++i){
+        std::cin >> A >> B;
+        std::cout << "Case #" << i + 1 << ": " << A + B << std::endl;
+    }
+}
