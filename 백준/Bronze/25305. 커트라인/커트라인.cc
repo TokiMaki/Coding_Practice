256184c7eb83b003736e54413a8ea2cf9647bc88
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-02-05 16:07:27 +0900

    [Bronze II] Title: 커트라인, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, k;
+
+	cin >> N >> k;
+
+	vector<int> v;
+
+	for (int i = 0; i < N; ++i) {
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
+	}
+
+	sort(v.begin(), v.end(), greater<>());
+
+	cout << v[k - 1] << endl;
+}
