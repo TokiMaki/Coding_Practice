80b1a0a5f1f99ed0bbd8a9bcb1af487336c84dce
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-02-07 17:15:41 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 308 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void star(int i, int j, int num)
+{
+    if ((i / num) % 3 == 1 && (j / num) % 3 == 1)
+        cout << " ";
+    else {
+        if (num / 3 == 0)
+            cout << "*";
+        else
+            star(i, j, num / 3);
+    }
+}
+int main() {
+    int N;
+    cin >> N;
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j)
+            star(i, j, N);
+        cout << "\n";
+    }
+}
