6c6c94523e2bb79ba7e5c25746c95a7ff8d94d8e
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-02-09 17:49:15 +0900

    다른 풀이 넣기

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드2 이진탐색풀이.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    // 출력 최적화
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int N, M;
+    vector<int> Nv;
+
+    cin >> N;
+    for (int i = 0; i < N; ++i) {
+        int tmp;
+        cin >> tmp;
+        Nv.push_back(tmp);
+        sort(Nv.begin(), Nv.end());
+    }
+
+    cin >> M;
+    for (int i = 0; i < M; ++i) {
+        int tmp;
+        cin >> tmp;
+        auto result = upper_bound(Nv.begin(), Nv.end(), tmp) - lower_bound(Nv.begin(), Nv.end(), tmp);
+        cout << result << " ";
+    }
+}
