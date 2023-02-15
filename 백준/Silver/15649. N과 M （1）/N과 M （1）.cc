5a16a1fa73db7f18adc5bd1fa91c95f250bf41b6
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-02-15 17:47:02 +0900

    [Silver III] Title: N과 M (1), Time: 176 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <set>
+#include <algorithm>
+
+using namespace std;
+
+int result = 0;
+
+bool isduplication(int* num, int x) {
+	set<int> s;
+	for (int i = 0; i < x + 1; ++i) {
+		s.insert(num[i]);
+	}
+	return (s.size() == x + 1);
+}
+
+void dfs(int N, int M, int x) {
+	static int num[8];
+	if (x == M) {
+		for (int i = 0; i < M; ++i) {
+			cout << num[i] << " ";
+		}
+		cout << "\n";
+		return;
+	}
+	else {
+		for (int i = 1; i <= N; ++i) {
+			num[x] = i;
+			if (isduplication(num, x)) {
+				dfs(N, M, x + 1);
+			}
+		}
+	}
+}
+
+int main() {
+	// 출력 최적화
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+	dfs(N, M, 0);
+}
