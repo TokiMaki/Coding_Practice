6f8da2f8b779dbeedba2619e26a64799611568c0
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-02-16 23:12:44 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -4,29 +4,21 @@
 #include <algorithm>
 
 using namespace std;
-set<vector<int>> withoutv;
 
-void d(int N, int M, int x) {
+void d(int N, int M, int index, int x) {
 	static bool b[8] = { false, };
 	static vector<int> v;
 	if (x >= M) {
-		vector<int> tmp;
-		for (int i : v)
-			tmp.push_back(i);
-		sort(tmp.begin(), tmp.end());
-		if (withoutv.end() == withoutv.find(tmp)) {
-			withoutv.insert(tmp);
 			for (int i : v)
 				cout << i + 1 << " ";
 			cout << "\n";
-		}
 		return;
 	}
-	for (int i = 0; i < N; ++i) {
+	for (int i = index; i < N; ++i) {
 		if (b[i] == false) {
 			v.push_back(i);
 			b[i] = true;
-			d(N, M, x + 1);
+			d(N, M, i + 1, x + 1);
 			v.pop_back();
 			b[i] = false;
 		}
@@ -40,5 +32,5 @@ int main() {
 
 	int N, M;
 	cin >> N >> M;
-	d(N, M, 0);
+	d(N, M, 0, 0);
 }
