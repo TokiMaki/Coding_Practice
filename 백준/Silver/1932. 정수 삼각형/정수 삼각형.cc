f95a29634799bb473b22e11344bec488c0a419a4
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-02-22 04:56:04 +0900

    [Silver I] Title: 정수 삼각형, Time: 40 ms, Memory: 3000 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -1,45 +1,38 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #include <climits>
 
 using namespace std;
 
 int triangle[501][501] = { 0, };
-int maxtriangle[501][501] = { 0, };
 
 int main() {
 	int N;
 	cin >> N;
-	vector<int> trianglev;
-	vector<int> maxtrianglev;
 	for (int i = 1; i <= N; ++i) {
 		for (int j = 0; j < i; ++j) {
 			int tmp;
 			cin >> tmp;
 			triangle[i][j] = tmp;
-			trianglev.push_back(tmp);
 		}
 	}
-	maxtriangle[1][0] = triangle[1][0];
-	maxtrianglev.push_back(trianglev[0]);
 	for (int i = 2; i <= N; ++i) {
 		for (int j = 0; j < i; ++j) {
 			if (j == 0) {
-				maxtriangle[i][j] = maxtriangle[i - 1][j] + triangle[i][j];
+				triangle[i][j] = triangle[i - 1][j] + triangle[i][j];
 			}
 			else if (j == i - 1) {
-				maxtriangle[i][j] = maxtriangle[i - 1][j - 1] + triangle[i][j];
+				triangle[i][j] = triangle[i - 1][j - 1] + triangle[i][j];
 			}
 			else {
-				maxtriangle[i][j] = max(maxtriangle[i - 1][j - 1], maxtriangle[i - 1][j]) + triangle[i][j];
+				triangle[i][j] = max(triangle[i - 1][j - 1], triangle[i - 1][j]) + triangle[i][j];
 			}
 		}
 	}
 	int maxcost = INT_MIN;
 
 	for (int i = 0; i < N; ++i) {
-		maxcost = max(maxcost, maxtriangle[N][i]);
+		maxcost = max(maxcost, triangle[N][i]);
 	}
 	cout << maxcost;
 }
