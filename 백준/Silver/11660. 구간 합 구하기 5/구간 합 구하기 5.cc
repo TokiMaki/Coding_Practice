18cff403a279a59a3d3e718afd5fe845ad6186bb
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-02-26 22:11:22 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 348 ms, Memory: 6124 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int table[1025][1025] = { 0, };
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int N, M;
+	cin >> N >> M;
+
+
+	for (int i = 1; i <= N; ++i) {
+		for (int j = 1; j <= N; ++j) {
+			int tmp;
+			cin >> tmp;
+			table[i][j] += tmp;
+			if (j + 1 <= N)
+				table[i][j + 1] += table[i][j];
+		}
+	}
+
+	int x1, x2;
+	int y1, y2;
+	for (int i = 0; i < M; ++i) {
+		cin >> x1 >> x2 >> y1 >> y2;
+		pair<int, int> xminmax = minmax(x1, y1);
+		pair<int, int> yminmax = minmax(x2, y2);
+		int result = 0;
+		for (int j = xminmax.first; j <= xminmax.second; ++j) {
+			if (yminmax.first - 1 >= 0)
+				result += (table[j][yminmax.second] - table[j][yminmax.first - 1]);
+			else
+				result += table[j][yminmax.second];
+		}
+		cout << result << "\n";
+	}
+}
