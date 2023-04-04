7023963d644bd9218e97742655a7cc190567b217
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-04-04 18:46:55 +0900

    피로도 계산기

---
+++ 장난감/DNF fatigue calculator.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+int Dungeon = 5;
+int leftfatigues;
+int Sum = 0;
+vector<int> Dungeonfatigues;
+vector<int> v;
+set<vector<int>> s;
+
+void backtracking(int n) {
+	if (Sum > leftfatigues - 1)
+		return;
+	if (Sum == leftfatigues - 1) {
+		vector<int> tmp;
+		for (int i : v)
+			tmp.push_back(i);
+		sort(tmp.begin(), tmp.end());
+		s.insert(tmp);
+	}
+	for (int i = 0; i < Dungeon; ++i) {
+		if (Sum + Dungeonfatigues[i] <= leftfatigues) {
+			Sum += Dungeonfatigues[i];
+			v.push_back(Dungeonfatigues[i]);
+			backtracking(n + 1);
+			Sum -= Dungeonfatigues[i];
+			v.pop_back();
+		}
+	}
+}
+
+int main() {
+	cout << "│던전명              │ 직 │ 풀 │" << endl;
+	cout << "│백색의 땅           │ 7  │ 15 │" << endl;
+	cout << "│베리콜리스          │ 6  │ 6  │" << endl;
+	cout << "│케니언 힐           │ 6  │ 9  │" << endl;
+	cout << "│퀸 팔트             │ 6  │ 12 │" << endl;
+	cout << "│왕의 요람           │ 7  │ 9  │" << endl;
+	cout << "│헤블론의 예언소     │ 7  │ 11 │" << endl;
+	cout << "│나사우 삼림         │ 8  │ 9  │" << endl;
+	cout << "│이터널 플레임 연구소│ 8  │ 11 │" << endl;
+	while (true) {
+		cout << "남은 피로도 : ";
+		cin >> leftfatigues;
+
+		Dungeonfatigues.push_back(6);
+		Dungeonfatigues.push_back(7);
+		Dungeonfatigues.push_back(8);
+		Dungeonfatigues.push_back(9);
+		Dungeonfatigues.push_back(12);
+
+		backtracking(0);
+		for (vector<int> v : s) {
+			for (int i : v)
+				cout << i << " ";
+			cout << endl;
+		}
+	}
+}
