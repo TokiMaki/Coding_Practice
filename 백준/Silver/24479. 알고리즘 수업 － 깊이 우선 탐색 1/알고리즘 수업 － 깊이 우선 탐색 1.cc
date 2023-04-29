f864817158c7a72179ea48bb8e9e1bf0c3da6b3b
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-04-29 17:14:31 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 84 ms, Memory: 10932 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> graph[100001];
+int visited[100001] = { 0, };
+
+void dfs(int start)
+{
+	static int cnt = 0;
+	visited[start] = ++cnt;
+	for(int i : graph[start])
+	{
+		if (visited[i] == 0)
+			dfs(i);
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N, M, R;
+	cin >> N >> M >> R;
+	for(int i = 0; i < M; ++i)
+	{
+		int s, e;
+		cin >> s >> e;
+		graph[s].push_back(e);
+		graph[e].push_back(s);
+	}
+
+	for (int i = 1; i <= N; ++i)
+		sort(graph[i].begin(), graph[i].end());
+
+	dfs(R);
+	for(int i = 1; i <= N; ++i)
+	{
+		cout << visited[i] << "\n";
+	}
+}
