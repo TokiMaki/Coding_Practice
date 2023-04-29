0c16cadcc8e9645f0bf55a00c493ceae8516a9cc
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-04-29 17:57:09 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 84 ms, Memory: 8840 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+using namespace std;
+
+vector<int> graph[100001];
+int visited[100001] = { 0, };
+
+void bfs(int start)
+{
+	static int cnt = 0;
+	queue<int> q;
+	visited[start] = ++cnt;
+	q.push(start);
+	while(!q.empty())
+	{
+		int s = q.front();
+		q.pop();
+		for(int i : graph[s])
+		{
+			if (visited[i] == 0) {
+				visited[i] = ++cnt;
+				q.push(i);
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int N, M, R;
+	cin >> N >> M >> R;
+	for(int i = 0; i < M; ++i)
+	{
+		int s, e;
+		cin >> s >> e;
+		graph[s].push_back(e);
+		graph[e].push_back(s);
+	}
+
+	for (int i = 1; i <= N; ++i)
+		sort(graph[i].begin(), graph[i].end(), greater<int>());
+
+	bfs(R);
+	for(int i = 1; i <= N; ++i)
+	{
+		cout << visited[i] << "\n";
+	}
+}
