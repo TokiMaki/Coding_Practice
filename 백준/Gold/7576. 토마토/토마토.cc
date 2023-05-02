32afbd2785a72fb68a9496ee340b8c9dabf17329
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-05-02 17:37:43 +0900

    [Gold V] Title: 토마토, Time: 80 ms, Memory: 12492 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <climits>
+#include <queue>
+
+using namespace std;
+
+int M, N;
+int box[1000][1000] = {0, };
+int answer = -1;
+queue<pair<int, pair<int, int>>> q;
+int dir_x[4] = { 1, 0, -1, 0 };
+int dir_y[4] = { 0, 1, 0, -1 };
+
+void bfs()
+{
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < M; ++j)
+		{
+			if (box[i][j] == 1)
+				q.push({ 1, { j, i } });
+		}
+	}
+	while(!q.empty())
+	{
+		int cnt = q.front().first;
+		int curr_x = q.front().second.first;
+		int curr_y = q.front().second.second;
+		q.pop();
+		for(int i = 0; i < 4; ++i)
+		{
+			int next_cnt = cnt + 1;
+			int next_x = curr_x + dir_x[i];
+			int next_y = curr_y + dir_y[i];
+			if (next_x < 0 || next_x >= M || next_y < 0 || next_y >= N)
+				continue;
+			if (box[next_y][next_x] != -1 && box[next_y][next_x] != 1
+				&& (box[next_y][next_x] > next_cnt || box[next_y][next_x] == 0))
+			{
+				box[next_y][next_x] = next_cnt;
+				q.push({ next_cnt, { next_x, next_y } });
+			}
+		}
+	}
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cin >> M >> N;
+	for(int i = 0; i < N; ++i)
+	{
+		for(int j = 0; j < M; ++j)
+		{
+			cin >> box[i][j];
+		}
+	}
+
+	bfs();
+
+	int trigger = false;
+	for (int i = 0; i < N; ++i)
+	{
+		for (int j = 0; j < M; ++j)
+		{
+			if (box[i][j] == 0) {
+				answer = -1;
+				trigger = true;
+				break;
+			}
+			else
+				answer = max(answer, box[i][j] - 1);
+		}
+		if (trigger == true)
+			break;
+	}
+	cout << answer << endl;
+}
