0a1b466a9a952305cf4bcbb8e8df8c51c1045407
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-05-24 17:38:52 +0900

    [level 1] Title: 최소직사각형, Time: 1.02 ms, Memory: 5.02 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<vector<int>> sizes) {
+    int answer = 0;
+    for(vector<int> &v : sizes) {
+        if (v[0] < v[1])
+            swap(v[0], v[1]);
+    }
+    int row = 0;
+    int columm = 0;
+    for(vector<int> v : sizes) {
+        row = max(row, v[0]);
+        columm = max(columm, v[1]);
+    }
+    answer = row * columm;
+    return answer;
+}
