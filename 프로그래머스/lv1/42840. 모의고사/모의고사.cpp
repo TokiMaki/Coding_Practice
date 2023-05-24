45bb84467d8d065e2fd730240e0eb50ff506dfa1
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-05-24 18:28:10 +0900

    [level 1] Title: 모의고사, Time: 0.23 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(vector<int> answers) {
+    vector<int> answer;
+    vector<vector<int>> person{{1, 2, 3, 4, 5}, {2, 1, 2, 3, 2, 4, 2, 5}, {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}};
+    vector<int> answernum = {0, 0, 0};
+    
+    for(int i = 0; i < answers.size(); ++i) {
+        for(int j = 0; j < 3; ++j) {
+            if (answers[i] == person[j][i % person[j].size()])
+                answernum[j]++;
+        }
+    }
+    
+    int answermax = *max_element(answernum.begin(), answernum.end());
+    
+    for(int i = 0; i < answernum.size(); ++i){
+        if (answermax == answernum[i])
+            answer.push_back(i + 1);
+    }
+        
+    return answer;
+}
