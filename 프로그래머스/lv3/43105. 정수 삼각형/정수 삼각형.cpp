98633ba13a8376085571ebe966dd41f4f1aa0602
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-06-06 17:16:50 +0900

    [level 3] Title: 정수 삼각형, Time: 1.08 ms, Memory: 8.49 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/43105. 정수 삼각형/정수 삼각형.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int>> triangle) {
+    int answer = 0;
+    int size = triangle.size();
+    vector<vector<int>> dp(triangle.size());
+    dp[0].push_back(triangle[0][0]);
+    for (int i = 1; i < triangle.size(); ++i) {
+        for (int j = 0; j < triangle[i].size(); ++j) {
+            int left = 0, right = 0;
+            if (j - 1 >= 0)
+                left = dp[i - 1][j - 1] + triangle[i][j];
+            if (j < triangle[i - 1].size())
+                right = dp[i - 1][j] + triangle[i][j];
+            dp[i].push_back(max(left, right));
+        }
+    }
+    answer = *max_element(dp[size - 1].begin(), dp[size - 1].end());
+    return answer;
+}
