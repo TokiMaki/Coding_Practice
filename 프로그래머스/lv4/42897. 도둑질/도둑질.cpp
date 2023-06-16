9f63a57125d2b9ea3918ffe87e495b56e380d904
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-06-16 18:08:23 +0900

    [level 4] Title: 도둑질, Time: 7.46 ms, Memory: 38.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv4/42897. 도둑질/도둑질.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> money) {
+    int answer = 0;
+    vector<int> dp1(money.size());
+    dp1[0] = money[0];
+    dp1[1] = money[0];
+    vector<int> dp2(money.size());
+    dp2[0] = 0;
+    dp2[1] = money[1];
+    for(int i = 2; i < money.size() - 1; ++i) {
+        dp1[i] = max(dp1[i - 2] + money[i], dp1[i - 1]);
+    }
+    for(int i = 2; i < money.size(); ++i) {
+        dp2[i] = max(dp2[i - 2] + money[i], dp2[i - 1]);
+    }
+    answer = max(dp1[money.size() - 2], dp2[money.size() - 1]);
+    return answer;
+}
