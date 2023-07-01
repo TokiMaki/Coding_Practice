1e3682cfbff5311a1ca74d29bf23c892f3daa193
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-07-01 21:04:49 +0900

    Update 전화번호 목록.cpp

--- 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/lv2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -2,35 +2,23 @@
 #include <vector>
 #include <unordered_map>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
 
-    unordered_map<string, vector<int>> um;
+    unordered_map<string, int> um;
 
-    for (int i = 0; i < phone_book.size(); ++i) {
-        for (int j = 1; j <= phone_book[i].length(); ++j) {
-            string nexts;
-            nexts.assign(phone_book[i], 0, j);
-            auto findnext = um.find(nexts);
-            if (findnext == um.end()) {
-                vector<int> v;
-                v.push_back(i);
-                um.insert({ nexts, v });
-            }
-            else {
-                findnext->second.push_back(i);
-            }
-        }
+    for (string s : phone_book) {
+        um.insert({ s, 1 });
     }
 
-    for (int i = 0; i < phone_book.size(); ++i) {
-        auto findnext = um.find(phone_book[i]);
-        if (findnext != um.end()) {
-            if (find_if(findnext->second.begin(), findnext->second.end(), [&i](int ii){return i != ii;}) 
-                != findnext->second.end()) {
+    for (string s : phone_book) {
+        for (int i = 1; i <= s.length(); ++i) {
+            string temp = s.substr(0, i);
+            if (um[temp] && temp != s) {
                 answer = false;
                 break;
             }
