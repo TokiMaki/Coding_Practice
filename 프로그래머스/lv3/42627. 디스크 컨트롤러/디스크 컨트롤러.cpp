e947515facd7cba56c63536608e4238e2f2cb871
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-07-25 16:42:55 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.cpp
@@ -10,28 +10,30 @@ bool comp(pair<int, int> a, pair<int, int> b){
 
 int solution(vector<vector<int>> jobs) {
     int answer = 0;
-    priority_queue <pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    priority_queue <pair<int, int>, vector<pair<int, int>>, decltype(&comp)> time_pq(comp);
-    for (auto a : jobs)
-        pq.emplace(a[0], a[1]);
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> p_q;
+    auto comp = [](pair<int, int> a, pair<int, int> b){ return a.second > b.second; };
+    priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(comp)> timep_q(comp);
+    for(vector<int> job : jobs)
+        p_q.emplace(job[0], job[1]);
     int now = 0;
-    while (!pq.empty() || !time_pq.empty()) {
-        int wait = pq.top().first;
-        int volume = pq.top().second;
-        if (!pq.empty() && wait <= now) {
-            time_pq.emplace(wait, volume);
-            pq.pop();
-            continue;
+    while(!p_q.empty() || !timep_q.empty()) {
+        if (!p_q.empty()) {
+            pair<int, int> job = p_q.top();
+            if (now >= job.first) {
+                timep_q.push(job);
+                p_q.pop();
+                continue;
+            }
         }
-        if (!time_pq.empty()) {
-            int timewait = time_pq.top().first;
-            int timevolume = time_pq.top().second;
-            now += timevolume;
-            answer += now - timewait;
-            time_pq.pop();
+        if (!timep_q.empty()) {
+            pair<int, int> job = timep_q.top();
+            now += job.second;
+            answer += now - job.first;
+            timep_q.pop();
         }
         else {
-            now = wait;
+            pair<int, int> job = p_q.top();
+            now = job.first;
         }
     }
     return answer / jobs.size();
