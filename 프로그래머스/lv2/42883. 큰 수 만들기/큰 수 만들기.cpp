b81d16b2e72f9239e7d490516b100ed404a20694
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-07-28 16:44:51 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.01 ms, Memory: 4.22 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.cpp
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.cpp
@@ -5,18 +5,17 @@ using namespace std;
 
 string solution(string number, int k) {
     string answer = "";
-    for (int i = 0; i < k; ++i) {
-        bool deletecheck = false;
-        for (int j = 0; j < number.size() - 1; ++j) {
+    for(int i = 0; i < k; ++i) {
+        bool check = false;
+        for(int j = 0; j < number.size() - 1; ++j) {
             if (number[j] < number[j + 1]) {
-                deletecheck = true;
                 number.erase(number.begin() + j);
+                check = true;
                 break;
             }
         }
-        if (deletecheck == false) {
+        if (check == false)
             number.erase(number.end() - 1);
-        }
     }
     answer = number;
     return answer;
