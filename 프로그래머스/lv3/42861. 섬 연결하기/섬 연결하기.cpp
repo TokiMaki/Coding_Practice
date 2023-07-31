1410b621799c7fca4604c6ca5ff8121c6c2b65ec
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-07-31 17:46:35 +0900

    [level 3] Title: 섬 연결하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.cpp
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.cpp
@@ -4,37 +4,37 @@
 
 using namespace std;
 
-int parent[100];
+int parant[100];
 
-int findparent(int x) {
-    if (x == parent[x])
+int findparant(int x) {
+    if (parant[x] == x)
         return x;
-    return parent[x] = findparent(parent[x]);
+    return parant[x] = findparant(parant[x]);
 }
 
-int unionparent(int a, int b) {
-    a = findparent(a);
-    b = findparent(b);
+void unionparant(int a, int b) {
+    a = findparant(a);
+    b = findparant(b);
+    
     if (a < b)
-        return parent[b] = a;
+        parant[b] = a;
     else
-        return parent[a] = b;
+        parant[a] = b;
 }
 
 int solution(int n, vector<vector<int>> costs) {
     int answer = 0;
-
-    for(int i = 0; i < 100; ++i) { 
-        parent[i] = i;
-    }
+    
+    for(int i = 0; i < 100; ++i)
+        parant[i] = i;
     
     sort(costs.begin(), costs.end(), [](vector<int> a, vector<int> b){
         return a[2] < b[2];
     });
     
     for(vector<int> cost : costs) {
-        if (findparent(cost[0]) != findparent(cost[1])) {
-            unionparent(cost[0], cost[1]);
+        if (findparant(cost[0]) != findparant(cost[1])) {
+            unionparant(cost[0], cost[1]);
             answer += cost[2];
         }
     }
