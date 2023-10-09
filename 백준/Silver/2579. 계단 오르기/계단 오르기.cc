96a975e2998e2718dfd0691cd81a4654c57e44cc
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-10-09 17:45:05 +0900

    [Silver III] Title: 계단 오르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.cc
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.cc
@@ -1,27 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
 
-int main() {
+int dp[301];
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	vector<int> v;
+
 	int N;
 	cin >> N;
-	int sum = 0;
-	vector<int> v;
-	vector<int> dp;
-	for (int i = 0; i < N; ++i) {
-		int tmp;
-		cin >> tmp;
-		v.push_back(tmp);
-	}
-	dp.push_back(v[0]);
-	dp.push_back(v[0] + v[1]);
-	dp.push_back(max(v[0] + v[2], v[1] + v[2]));
-
-	for (int i = 3; i < N; ++i) {
-		dp.push_back(max(dp[i - 2] + v[i], dp[i - 3] + v[i - 1] + v[i]));
+	for(int i = 0; i < N; ++i)
+	{
+		int temp;
+		cin >> temp;
+		v.push_back(temp);
 	}
 
+	dp[0] = v[0];
+	dp[1] = v[0] + v[1];
+	dp[2] = max(v[0] + v[2], v[1] + v[2]);
+	for(int i = 3; i < N; ++i)
+	{
+		dp[i] = max(dp[i - 2] + v[i], dp[i - 3] + v[i - 1] + v[i]);
+	}
 	cout << dp[N - 1];
 }
