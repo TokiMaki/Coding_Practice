fe91c7525025d54a6c5c5d6d9b5de2af5b14f77c
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-10-10 17:02:50 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -4,26 +4,34 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 	int N;
+	int answer = 0;
+
 	cin >> N;
-	vector<int> v(N);
-	vector<int> maxv(N);
-	int answer = 1;
-	for (int i = 0; i < N; ++i) {
-		int tmp;
-		cin >> tmp;
-		v[i] = tmp;
-	}
-	maxv[0] = 1;
-	for (int i = 1; i < N; ++i) {
-		maxv[i] = 1;
-		for (int j = i - 1; j >= 0; --j) {
-			if (v[i] > v[j])
-				maxv[i] = max(maxv[i], maxv[j] + 1);
-		}
-		answer = max(answer, maxv[i]);
+
+	vector<int> A(N);
+	vector<int> dp(N);
+
+
+	for(int i = 0; i < N; ++i)
+	{
+		cin >> A[i];
 	}
 
+	dp[0] = 1;
+	for(int i = 1; i < N; ++i)
+	{
+		dp[i] = 1;
+		for(int j = i - 1; j >= 0; --j)
+		{
+			if (A[i] > A[j])
+			{
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+	}
+	answer = *max_element(dp.begin(), dp.end());
 	cout << answer;
 }
