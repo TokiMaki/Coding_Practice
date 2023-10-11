57da3e3c642f12d1cbcd296a56a2f8b07125e29c
양현석/Yang Hyeon Seok <40135375+TokiMaki@users.noreply.github.com>
2023-10-11 18:32:20 +0900

    [Silver I] Title: 숨바꼭질, Time: 0 ms, Memory: 2544 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.cc
@@ -1,53 +1,52 @@
 #include <iostream>
-#include <climits>
+#include <vector>
 #include <queue>
+#include <climits>
 
 using namespace std;
 
 int N, K;
-int answer = INT_MAX;
+
 int visited[100001];
 
-void bfs(int start, int dis, int cnt)
+int bfs()
 {
-	queue<pair<int,int>> q;
-	q.push({ start, cnt } );
-	while(!q.empty())
+	queue<pair<int, int>> p;
+	
+	p.emplace(0, N);
+	while(!p.empty())
 	{
-		int s = q.front().first;
-		int c = q.front().second;
-		q.pop();
-		if (s == K) {
-			answer = min(answer, c);
-			continue;
+		int cnt = p.front().first;
+		int now = p.front().second;
+		p.pop();
+		if (now == K)
+			return cnt;
+
+		if (now - 1 >= 0) {
+			if (visited[now - 1] > cnt) {
+				visited[now - 1] = cnt;
+				p.emplace(cnt + 1, now - 1);
+			}
 		}
-		if (s * 2 <= 100000 && (visited[s * 2] > c))
-		{
-			visited[s * 2] = c;
-			q.push({ s * 2, c + 1} );
+		if (now + 1 <= 100000) {
+			if (visited[now + 1] > cnt) {
+				visited[now + 1] = cnt;
+				p.emplace(cnt + 1, now + 1);
+			}
 		}
-		if (s + 1 <= 100000 && (visited[s + 1] > c))
-		{
-			visited[s + 1] = c;
-			q.push({ s + 1, c + 1} );
-		}
-		if (s - 1 >= 0 && (visited[s - 1] > c))
-		{
-			visited[s - 1] = c;
-			q.push({ s - 1, c + 1} );
+
+		if (now * 2 <= 100000) {
+			if (visited[now * 2] > cnt) {
+				visited[now * 2] = cnt;
+				p.emplace(cnt + 1, now * 2);
+			}
 		}
 	}
 }
 
 int main()
 {
-	cin.tie(NULL);
-	ios_base::sync_with_stdio(false);
-
 	cin >> N >> K;
-
 	fill_n(visited, 100001, INT_MAX);
-
-	bfs(N, abs(K - N), 0);
-	cout << answer;
+	cout << bfs();
 }
